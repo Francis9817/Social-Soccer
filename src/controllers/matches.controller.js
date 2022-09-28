c6ea496cc441e486c20d1c888fef71625db2af79
Fernando Rivera <ferchofercho933@gmail.com>
2022-09-27 20:00:50 -0500

    changes

--- src/controllers/matches.controller.js
+++ src/controllers/matches.controller.js
@@ -6,4 +6,8 @@ Matches.getListMatches = (req, res) => {
     res.render('pages/matches/list-matches');
 };
 
+ Matches.getListPlayersTeamMatches = (req, res) =>{
+    res.render('pages/matches/list-players-team-matches')
+
+};
 module.exports=Matches
--- src/routes/matches.routes.js
+++ src/routes/matches.routes.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const getListMatches = require('../controllers/matches.controller').getListMatches;
+const matchesController = require('../controllers/matches.controller');
+
 
 
 const pool = require('../config/database');
 
 
-router.get('/list-matches', getListMatches);
+router.get('/list-matches', matchesController.getListMatches);
+router.get('/list-matches', matchesController.getListPlayersTeamMatches);
 
 
-router.get('/list-players-team-matches', (req, res) =>{
-    res.render('pages/matches/list-players-team-matches')
 
-});
 
 
 
