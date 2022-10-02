a82143eda19c92fca8d790337f176e64044f3eb6
Fernando Rivera <ferchofercho933@gmail.com>
2022-10-01 20:41:53 -0500

    changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    res.render('index');
+    res.render('pages/dashboard');
 });
 
 module.exports = router;
